1eee92797a44ed2b560fbfcb18ca362546393c00
Bruno Neves <nevesbruno814@gmail.com>
2022-06-07 11:16:23 -0400

    Mudular implementado

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:asuka/asuka.dart' as asuka;
+import 'package:job_timer/app/mudules/core/ui/core_ui_config.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Modular.setInitialRoute('/');
+    return MaterialApp.router(
+      title: 'Job Timer',
+      builder: asuka.builder,
+      theme: CoreUiConfig.theme,
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
+    );
+  }
+}
---
+++ lib/app/mudules/core/ui/core_ui_config.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+class CoreUiConfig {
+  CoreUiConfig._();
+
+  static ThemeData get theme => ThemeData(
+    // primaryColor: Color();
+  );
+}
